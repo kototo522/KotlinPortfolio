c2f3cb74eda386e81ffb9371f3bcf02dd8376d96
shu20031026 <shu20031026@gmail.com>
2021-07-11 12:15:29 +0900

    emotionへの対応

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
-import React from "react";
 import "twin.macro";
+import "twin.macro";
+import { css } from "@emotion/react";
+
+const myStyle = css`
+  color: red;
+`;
 
 const hello = () => {
   return (
@@ -7,6 +12,7 @@ const hello = () => {
       <button tw="px-4 py-2 border-4 border-green-400 border-solid rounded focus:outline-none">
         tw prop
       </button>
+      <div css={myStyle}>Page1</div>
     </div>
   );
 };
