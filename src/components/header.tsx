0f145de0817e3fefe038c1a9c2bcca478b21fb75
kototo522 <huyejiben@gmail.com>
2021-09-03 14:24:12 +0900

    add: headerの追加

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,35 @@
 import React from "react"
+import "twin.macro";
+import "twin.macro";
+import { css } from "@emotion/react";
+
+const container = css`
+    position: sticky;
+    top: 5px;
+    display: flex;
+`;
+
+const headerButton = css`
+    padding-right: 36px;
+    line-height: 80px;
+    &:first-child{
+        margin-right:auto;
+        padding-left: 36px;
+    }
+    @media (max-width: 420px) {
+        padding-right: 24px;
+    }
+`;
+
 
 const header = () => {
-  return (
-    <div/>
-  );
+    return (
+        <header css={container}>
+            <button css={headerButton}>Logo</button>
+            <button css={headerButton}>Profile</button>
+            <button css={headerButton}>Skills</button>
+            <button css={headerButton}>Works</button>
+        </header >
+    );
 };
 export default header;
