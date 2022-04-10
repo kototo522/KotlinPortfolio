25f572b16e7bf2dbcfeaad619bb2645d8ac4a320
kototo522 <huyejiben@gmail.com>
2022-04-10 23:25:22 +0900

    add: errorの解決&テキストの変更

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { useInView } from 'react-intersection-observer';
+import { useInView } from "react-intersection-observer";
 import Kotlin from "./kotlin";
 import { css, keyframes } from "@emotion/react";
 
 const container = css`
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 `;
 
 const popUp = keyframes`
@@ -22,64 +22,74 @@ const popUp = keyframes`
 `;
 
 const profileCard = css`
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
-    width: 940px;
-    border: 1px solid #000000;
-    box-shadow: 12px 12px 0 #9C1919;
-    animation: ${popUp} 2s ease-out both;
-    @media (max-width: 940px) {
-        width: calc(100% - 50px);
-    }
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 940px;
+  border: 1px solid #000000;
+  box-shadow: 12px 12px 0 #9c1919;
+  animation: ${popUp} 2s ease-out both;
+  @media (max-width: 940px) {
+    width: calc(100% - 50px);
+  }
 `;
 
 const text = css`
-    margin: 0 auto;
-    line-height: 2em;
-    font-size: 22px;
+  margin: 0 auto;
+  line-height: 2em;
+  font-size: 22px;
 `;
 
 const profile = () => {
-    const myBirthDay = {
-        year: 2004,
-        month: 5,
-        date: 22
-    };
-    const birthDate = new Date(myBirthDay.year, myBirthDay.month - 1, myBirthDay.date);
-    const y2 = birthDate.getFullYear().toString().padStart(4, '0');
-    const m2 = (birthDate.getMonth() + 1).toString().padStart(2, '0');
-    const d2 = birthDate.getDate().toString().padStart(2, '0');
-    const today = new Date();
-    const y1 = today.getFullYear().toString().padStart(4, '0');
-    const m1 = (today.getMonth() + 1).toString().padStart(2, '0');
-    const d1 = today.getDate().toString().padStart(2, '0');
-    const age = Math.floor((Number(y1 + m1 + d1) - Number(y2 + m2 + d2)) / 10000);
-    const [ref, inView] = useInView({
-        rootMargin: '-100px',
-        triggerOnce: true,
-    })
+  const myBirthDay = {
+    year: 2004,
+    month: 5,
+    date: 22,
+  };
+  const birthDate = new Date(
+    myBirthDay.year,
+    myBirthDay.month - 1,
+    myBirthDay.date
+  );
+  const y2 = birthDate.getFullYear().toString().padStart(4, "0");
+  const m2 = (birthDate.getMonth() + 1).toString().padStart(2, "0");
+  const d2 = birthDate.getDate().toString().padStart(2, "0");
+  const today = new Date();
+  const y1 = today.getFullYear().toString().padStart(4, "0");
+  const m1 = (today.getMonth() + 1).toString().padStart(2, "0");
+  const d1 = today.getDate().toString().padStart(2, "0");
+  const age = Math.floor((Number(y1 + m1 + d1) - Number(y2 + m2 + d2)) / 10000);
+  const [ref, inView] = useInView({
+    rootMargin: "-100px",
+    triggerOnce: true,
+  });
 
-    return (
-        <div>
-            <div ref={ref} css={container}>
-                {inView && (
-                    <div css={profileCard}>
-                        <Kotlin />
-                        <p css={text}>
-                            Name: Kotoha Yoshimoto<br />
-                            Age: {age}<br />
-                            Birthday: 2004/5/22<br />
-                            From: Fukuoka<br />
-                            Belongs: NITKIT<br />
-                            Bob: WebfrontEngineer, Designer<br />
-                        </p>
-                    </div>
-                )}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div ref={ref} css={container}>
+        {inView && (
+          <div css={profileCard}>
+            <Kotlin />
+            <p css={text}>
+              Name: Kotoha Yoshimoto
+              <br />
+              Age: {age}
+              <br />
+              Birthday: 2004/5/22
+              <br />
+              From: Fukuoka
+              <br />
+              Belongs: NITKIT
+              <br />
+              Bob: Web{"&"}androidEngineer, Designer
+              <br />
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 export default profile;
